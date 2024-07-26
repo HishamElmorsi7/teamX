d3b89b426524d80d79aa0390d8055969b37efa66
HishamElmorsi7 <74022699+HishamElmorsi7@users.noreply.github.com>
2024-07-26 19:53:33 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-console.log('z');
 console.log(
 'ss'
 )
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+console.log('z');
 
   @Get()
   getHello(): string {
